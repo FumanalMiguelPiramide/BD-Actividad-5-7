63bd44479b9237a99e6c164db8bbacd6e8fa1cd3
FumanalMiguelPiramie <200129@fppiramide.com>
2023-12-18 12:41:42 +0100

    correcion foranea entre gimnasio y entrenador

--- sentencias.sql
+++ sentencias.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE Gimnasio_San_Antonio;
-USE DATABASE Gimnasio_San_Antonio;
+USE Gimnasio_San_Antonio;
 
 CREATE TABLE Persona(
 dni varchar (9),
@@ -7,12 +7,18 @@ nombre varchar (50) NOT NULL,
 primer_apellido varchar (50) NOT NULL,
 CONSTRAINT pk_Persona PRIMARY KEY (dni)
 );
+CREATE TABLE Gimnasio(
+nombre varchar (50),
+CONSTRAINT pk_Gimnasio PRIMARY KEY (nombre),
+);
 CREATE TABLE Entrenador(
 id int AUTO_INCREMENT,
 dniPersona varchar (9) NOT NULL,
 salario float (8) NOT NULL,
+nombreGimnasio varchar (50),
 CONSTRAINT pk_Entrenador PRIMARY KEY (id),
-CONSTRAINT fk_Entrenador FOREIGN KEY (dniPersona) REFERENCES Persona (dni)
+CONSTRAINT fk_Entrenador FOREIGN KEY (dniPersona) REFERENCES Persona (dni),
+CONSTRAINT fk_Entrenador_Gimnasio FOREIGN KEY (nombreGimnasio) REFERENCES Gimnasio (nombre)
 );
 CREATE TABLE Cliente(
 id int AUTO_INCREMENT,
@@ -21,12 +27,6 @@ salario float (8) NOT NULL,
 CONSTRAINT pk_Cliente PRIMARY KEY (id),
 CONSTRAINT fk_Cliente_Persona FOREIGN KEY (dniPersona) REFERENCES Persona (dni)
 );
-CREATE TABLE Gimnasio(
-nombre varchar (50),
-dniEntrenador varchar (9) NOT NULL,
-CONSTRAINT pk_Gimnasio PRIMARY KEY (nombre),
-CONSTRAINT fk_Gimnasio_Entrenador FOREIGN KEY (dniEntrenador) REFERENCES Entrenador (id)
-);
 CREATE TABLE Planificacion(
 codigo int (8),
 fechaCodigo varchar (10) NOT NULL,
@@ -34,19 +34,19 @@ CONSTRAINT pk_Planificacion PRIMARY KEY (codigo)
 );
 CREATE TABLE Cliente_Entrenador_Planificacion(
 id int AUTO_INCREMENT,
-dniCliente varchar (9) NOT NULL,
-dniEntrenador varchar (9) NOT NULL,
+idCliente int NOT NULL,
+idEntrenador int NOT NULL,
 codigoPlanficacion int (8) NOT NULL,
 CONSTRAINT pk_Cliente_Entrenador_Planificacion PRIMARY KEY (id),
-CONSTRAINT fk_Cliente_Entrenador_Planificacion_Cliente FOREIGN KEY (dniCliente) REFERENCES Cliente (id),
-CONSTRAINT fk_Cliente_Entrenador_Planificacion_Entrenador FOREIGN KEY (dniEntrenador) REFERENCES Entrenador (id),
+CONSTRAINT fk_Cliente_Entrenador_Planificacion_Cliente FOREIGN KEY (idCliente) REFERENCES Cliente (id),
+CONSTRAINT fk_Cliente_Entrenador_Planificacion_Entrenador FOREIGN KEY (idEntrenador) REFERENCES Entrenador (id),
 CONSTRAINT fk_Cliente_Entrenador_Planificacion_Planificacion FOREIGN KEY (codigoPlanficacion) REFERENCES Planificacion (codigo)
 );
 CREATE TABLE Entrenamiento(
 codigo int (8),
 fechaCodigo varchar (10) NOT NULL,
 codigoPlanficacion int (8) NOT NULL,
-CONSTRAINT pk_Entrenamiento PRIMARY KEY (codigo)
+CONSTRAINT pk_Entrenamiento PRIMARY KEY (codigo),
 CONSTRAINT fk_Entrenamiento_Planificacion FOREIGN KEY (codigoPlanficacion) REFERENCES Planificacion (codigo)
 );
 CREATE TABLE Ejercicio(
@@ -60,20 +60,20 @@ id int AUTO_INCREMENT,
 codigoEntrenamiento int (8) NOT NULL,
 nombreEjercicio varchar (50) NOT NULL,
 CONSTRAINT pk_Entrenamiento_Ejercicio PRIMARY KEY (id),
-CONSTRAINT fk_Entrenamiento_Ejercicio_Entrenamiento FOREIGN KEY codigoEntrenamiento REFERENCES Entrenamiento (codigo),
-CONSTRAINT fk_Entrenamiento_Ejercicio_Ejercicio FOREIGN KEY nombreEjercicio REFERENCES Ejercicio (nombre)
+CONSTRAINT fk_Entrenamiento_Ejercicio_Entrenamiento FOREIGN KEY (codigoEntrenamiento) REFERENCES Entrenamiento (codigo),
+CONSTRAINT fk_Entrenamiento_Ejercicio_Ejercicio FOREIGN KEY (nombreEjercicio) REFERENCES Ejercicio (nombre)
 );
 CREATE TABLE Material(
 nombre varchar (50),
 nombreEjercicio varchar (50) NOT NULL,
 CONSTRAINT pk_Material PRIMARY KEY (nombre),
-CONSTRAINT fk_Material_Ejercicio FOREIGN KEY nombreEjercicio REFERENCES Ejercicio (nombre)
+CONSTRAINT fk_Material_Ejercicio FOREIGN KEY (nombreEjercicio) REFERENCES Ejercicio (nombre)
 );
 CREATE TABLE Material_Ejercicio(
 id int AUTO_INCREMENT,
 nombreMaterial varchar (50) NOT NULL,
 nombreEjercicio varchar (50) NOT NULL,
 CONSTRAINT pk_Material_Ejercicio PRIMARY KEY (id),
-CONSTRAINT fk_Material_Ejercicio_Material FOREIGN KEY nombreMaterial REFERENCES Material (nombre),
-CONSTRAINT fk_Material_Ejercicio_Ejercicio FOREIGN KEY nombreEjercicio REFERENCES Ejercicio (nombre)
+CONSTRAINT fk_Material_Ejercicio_Material FOREIGN KEY (nombreMaterial) REFERENCES Material (nombre),
+CONSTRAINT fk_Material_Ejercicio_Ejercicio FOREIGN KEY (nombreEjercicio) REFERENCES Ejercicio (nombre)
 );
